90daff6b7c86d83df3dfc56279f3459a88bd69c3
Cl0ud-Sm3gaL <79073661+Cl0ud-Sm3gaL@users.noreply.github.com>
2021-04-29 17:07:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Test 1</title>
  </head>
  <body>
- <?php echo '<p>Oh man I don't know what i'm doing</p>'; ?> 
+ <?php echo "<p>Oh man I don't know what i'm doing</p>"; ?> 
  </body>
 </html>
